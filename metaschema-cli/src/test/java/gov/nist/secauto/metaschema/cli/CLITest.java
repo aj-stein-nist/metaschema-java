474ea3634cd8631eee8b384d3d668dc042136e30
A.J. Stein <alexander.stein@nist.gov>
2024-01-05 17:38:59 -0500

    Add QueryCommand back to command handler

--- metaschema-cli/src/test/java/gov/nist/secauto/metaschema/cli/CLITest.java
+++ metaschema-cli/src/test/java/gov/nist/secauto/metaschema/cli/CLITest.java
@@ -26,77 +26,85 @@
 
 package gov.nist.secauto.metaschema.cli;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import gov.nist.secauto.metaschema.cli.commands.QueryCommand;
 import gov.nist.secauto.metaschema.cli.processor.ExitCode;
 import gov.nist.secauto.metaschema.cli.processor.ExitStatus;
-import gov.nist.secauto.metaschema.cli.CLI;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Stream;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
+import nl.altindag.log.LogCaptor;
 
 /**
  * Unit test for simple CLI.
  */
 public class CLITest {
-	void evaluateResult(@NonNull ExitStatus status, @NonNull ExitCode expectedCode) {
-		status.generateMessage(true);
-		assertAll(() -> assertEquals(expectedCode, status.getExitCode(), "exit code mismatch"),
-				() -> assertNull(status.getThrowable(), "expected null Throwable"));
-	}
+  void evaluateResult(@NonNull ExitStatus status, @NonNull ExitCode expectedCode) {
+    status.generateMessage(true);
+    assertAll(() -> assertEquals(expectedCode, status.getExitCode(), "exit code mismatch"),
+        () -> assertNull(status.getThrowable(), "expected null Throwable"));
+  }
 
-	void evaluateResult(@NonNull ExitStatus status, @NonNull ExitCode expectedCode,
-			@NonNull Class<? extends Throwable> thrownClass) {
-		status.generateMessage(true);
-		Throwable thrown = status.getThrowable();
-		assert thrown != null;
-		assertAll(() -> assertEquals(expectedCode, status.getExitCode(), "exit code mismatch"),
-				() -> assertEquals(thrownClass, thrown.getClass(), "expected Throwable mismatch"));
-	}
+  void evaluateResult(@NonNull ExitStatus status, @NonNull ExitCode expectedCode,
+      @NonNull Class<? extends Throwable> thrownClass) {
+    status.generateMessage(true);
+    Throwable thrown = status.getThrowable();
+    assert thrown != null;
+    assertAll(() -> assertEquals(expectedCode, status.getExitCode(), "exit code mismatch"),
+        () -> assertEquals(thrownClass, thrown.getClass(), "expected Throwable mismatch"));
+  }
 
-	private static Stream<Arguments> providesValues() {
-		ExitCode noExpectedExceptionClass = null;
-		List<Arguments> values = new ArrayList<>();
-		values.add(Arguments.of(new String[] {}, ExitCode.INVALID_COMMAND, noExpectedExceptionClass));
-		values.add(Arguments.of(new String[] { "-h" }, ExitCode.OK, noExpectedExceptionClass));
-		values.add(Arguments.of(new String[] { "generate-schema", "--help" }, ExitCode.INVALID_COMMAND,
-				noExpectedExceptionClass));
-		values.add(Arguments.of(new String[] { "validate", "--help" }, ExitCode.OK, noExpectedExceptionClass));
-		values.add(Arguments.of(new String[] { "validate-content", "--help" }, ExitCode.INVALID_COMMAND,
-				noExpectedExceptionClass));
-		values.add(Arguments.of(
-				new String[] { "validate",
-						"../databind/src/test/resources/metaschema/fields_with_flags/metaschema.xml" },
-				ExitCode.OK, noExpectedExceptionClass));
-		values.add(Arguments.of(new String[] { "generate-schema", "--overwrite", "--as", "JSON",
-				"../databind/src/test/resources/metaschema/fields_with_flags/metaschema.xml",
-				"target/schema-test.json" }, ExitCode.OK, noExpectedExceptionClass));
-		values.add(Arguments.of(new String[] { "query", "-m", "module.xml", "-i", "content-instance.xml", "\"2 + 2\""}, ExitCode.INVALID_COMMAND, noExpectedExceptionClass));
-		return values.stream();
-	}
+  private static Stream<Arguments> providesValues() {
+    ExitCode noExpectedExceptionClass = null;
+    List<Arguments> values = new ArrayList<>();
+    values.add(Arguments.of(new String[] {}, ExitCode.INVALID_COMMAND, noExpectedExceptionClass));
+    values.add(Arguments.of(new String[] { "-h" }, ExitCode.OK, noExpectedExceptionClass));
+    values.add(Arguments.of(new String[] { "generate-schema", "--help" }, ExitCode.INVALID_COMMAND,
+        noExpectedExceptionClass));
+    values.add(Arguments.of(new String[] { "validate", "--help" }, ExitCode.OK, noExpectedExceptionClass));
+    values.add(Arguments.of(new String[] { "validate-content", "--help" }, ExitCode.INVALID_COMMAND,
+        noExpectedExceptionClass));
+    values.add(Arguments.of(
+        new String[] { "validate",
+            "../databind/src/test/resources/metaschema/fields_with_flags/metaschema.xml" },
+        ExitCode.OK, noExpectedExceptionClass));
+    values.add(Arguments.of(new String[] { "generate-schema", "--overwrite", "--as", "JSON",
+        "../databind/src/test/resources/metaschema/fields_with_flags/metaschema.xml",
+        "target/schema-test.json" }, ExitCode.OK, noExpectedExceptionClass));
+    return values.stream();
+  }
 
-	@ParameterizedTest
-	@MethodSource("providesValues")
-	void testAllCommands(@NonNull String[] args, @NonNull ExitCode expectedExitCode,
-			Class<? extends Throwable> expectedThrownClass) {
-		if (expectedThrownClass == null) {
-			evaluateResult(CLI.runCli(args), expectedExitCode);
-		} else {
-			evaluateResult(CLI.runCli(args), expectedExitCode, expectedThrownClass);
-		}
-	}
+  @ParameterizedTest
+  @MethodSource("providesValues")
+  void testAllCommands(@NonNull String[] args, @NonNull ExitCode expectedExitCode,
+      Class<? extends Throwable> expectedThrownClass) {
+    if (expectedThrownClass == null) {
+      evaluateResult(CLI.runCli(args), expectedExitCode);
+    } else {
+      evaluateResult(CLI.runCli(args), expectedExitCode, expectedThrownClass);
+    }
+  }
+
+  @Test
+  void testQueryCommand() {
+    LogCaptor logCaptor = LogCaptor.forClass(QueryCommand.class);
+    String[] args
+        = new String[] { "query", "-m", "../databind/src/test/resources/metaschema/fields_with_flags/metaschema.xml",
+            "-i",
+            "../databind/src/test/resources/metaschema/fields_with_flags/example.json", "3 + 4 + 5" };
+    CLI.runCli(args);
+    assertThat(logCaptor.getInfoLogs()).containsExactly("[12]");
+  };
 }
